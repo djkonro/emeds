ddc901cb33b5c71c1b2869a61f991ef261c07f08
djkonro <djkonro35@gmail.com>
2016-08-01 10:23:53 +0100

    Added EmedsItemListActivity

---
+++ app/src/main/java/com/emeds/android/emeds/EmedsItemListActivity.java
@@ -0,0 +1,34 @@
+package com.emeds.android.emeds;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class EmedsItemListActivity extends BaseActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        Rlayoutid = R.layout.activity_emeds;
+        super.onCreate(savedInstanceState);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new EmedsItemListFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
