2fdbf0f53da1561e6ba49d7e22aeee64daa6535c
djkonro <djkonro35@gmail.com>
2016-08-08 11:59:34 +0100

    Added EmedsItemDetailFragment

---
+++ app/src/main/java/com/emeds/android/emeds/EmedsItemDetailFragment.java
@@ -0,0 +1,11 @@
+package com.emeds.android.emeds;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by konro on 8/8/16.
+ */
+
+public class EmedsItemDetailFragment extends Fragment{
+
+}
