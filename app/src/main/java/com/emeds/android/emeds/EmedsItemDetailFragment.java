547b1c6f77c5053506f7595f9707c2b85dd0039f
djkonro <djkonro35@gmail.com>
2016-08-12 22:21:55 +0100

    Added CardView to EmedsItemDetailFragment

--- app/src/main/java/com/emeds/android/emeds/EmedsItemDetailFragment.java
+++ app/src/main/java/com/emeds/android/emeds/EmedsItemDetailFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -16,12 +17,14 @@ import android.widget.TextView;
 public class EmedsItemDetailFragment extends Fragment{
 
     private TextView mTextView1;
+    private ImageView mTitleImageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_emeds_detail, container, false);
         mTextView1 = (TextView) view.findViewById(R.id.detial_text_view1);
+        mTitleImageView = (ImageView) view.findViewById(R.id.card_image);
 
         Cursor cur = EmedsItemListFragment.scur;
         cur.moveToPosition(EmedsItemListFragment.spos);
@@ -29,6 +32,7 @@ public class EmedsItemDetailFragment extends Fragment{
         String str1 = cur.getString(
                 cur.getColumnIndex(EmedsItemListFragment.columntitle));
         mTextView1.setText(str1);
+        mTitleImageView.setImageResource(R.drawable.test);
 
         return view;
     }
